3ffb5b1d94c3acfa3bb82e306ba96711f1a4275f
Franverri <francoetche@gmail.com>
2021-10-29 20:01:29 -0300

    Finish JSI hw

---
+++ 01-JSI/demo/prueba.js
@@ -0,0 +1,22 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// ...
+
+
+for (var asdwqdqwdwqdcqdcqdad = 5; asdwqdqwdwqdcqdcqdad <= 10; asdwqdqwdwqdcqdcqdad++) {
+  console.log(asdwqdqwdwqdcqdcqdad);
+}
+
+console.log('Termine!')
+
+
+var num = 1;
+while(num <= 10) {
+  console.log(num);
+  num++;
+}
+console.log('Termine!')
